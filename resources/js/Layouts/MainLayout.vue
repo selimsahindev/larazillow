3a0eb787c3e358b86655771d0897ceb529d42c04
Selim Şahin <selimsahin.sns@gmail.com>
2023-03-12 05:54:36 +0300

    flash message problem solved

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,15 +1,25 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>&nbsp;
-    <Link href="/listing">Listings Page</Link>
-    <div>The page with time {{ timer }}</div>
+    <Link href="/listing/create">Create Listing</Link>&nbsp;
+
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
+
     <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
-import { ref } from "vue";
+import { computed } from 'vue';
+import { Link, usePage } from "@inertiajs/vue3";
 
-const timer = ref(0);
-setInterval(() => timer.value++, 1000);
+const flashSuccess = computed(() => usePage().props.flash.success);
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
