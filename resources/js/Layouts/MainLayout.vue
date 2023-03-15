cd48f9ccfd9f2d127eb9284283c054f3a4a9b373
Selim Şahin <selimsahin.sns@gmail.com>
2023-03-15 15:05:28 +0300

    added styles for create and edit forms

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
         <div class="container mx-auto">
             <nav class="p-4 flex items-center justify-between">
                 <div class="text-lg font-medium">
@@ -8,7 +8,7 @@
                 <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-centered">
                     <Link :href="route('listing.index')">Larazillow</Link>
                 </div>
-                <div class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">
+                <div class="btn-primary">
                     <Link :href="route('listing.create')">New Listing</Link>
                 </div>
             </nav>
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    <ListingAddress :listing="listing" />
+  <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+    <Box class="md:col-span-7 flex items-center w-full">
+      <div class="w-full text-center font-medium text-gray-500">
+        No images
+      </div>
+    </Box>
+    <div class="md:col-span-5 flex flex-col gap-4">
+      <Box>
+        <template #header>
+          Basic info
+        </template>
+        <Price :price="listing.price" class="text-2xl" />
+        <ListingSpace :listing="listing" class="text-lg hover:text-gray-500 dark:hover:text-gray-100" />
+        <ListingAddress :listing="listing" class="text-gray-500" />
+      </Box>
+      <Box>
+        <template #header>
+          Offer
+        </template>
+        Make an offer!
+      </Box>
+    </div>
+    <div class="text-lg font-medium rounded-md p-2">
+      <Link :href="route('listing.index')">Back</Link>
+    </div>
   </div>
 </template>
 
 <script setup>
 import ListingAddress from '@/Components/UI/ListingAddress.vue';
+import ListingSpace from '@/Components/UI/ListingSpace.vue';
+import Price from '@/Components/UI/Price.vue';
+import Box from '@/Components/UI/Box.vue';
+import { Link } from "@inertiajs/vue3";
+
 defineProps({
   listing: Object,
 });
